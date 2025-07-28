0041e2a898ab7444b619f26f0d54ddfcf900cd6a
gera <gera@computare.local>
2025-07-28 12:15:03 -0600

    testing with usd

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,9 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+   gasReporter: {
+    currency: 'USD',
+    etherscan: process.env.ETHERSCAN_API_KEY,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  }
 };
